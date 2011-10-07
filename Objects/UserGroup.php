d07f4912cb89aa1c598ab311f03da9bf7b02d832
dhaustov <dimon.nvkz@gmail.com>
2011-10-07 08:56:25 +0700

    Start to adding user groups

---
+++ Objects/UserGroup.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * User Groups. Related with table user_groups
+ *
+ * @author Dmitry G. Haustov
+ */
+class UserGroup {
+    var $id;
+    var $name;
+    var $idParentGroup;
+    var $idMasterUserAccount;
+    var $masterUserAccountRole;
+    var $status;
+    
+    var $relatedDocumentTemplates;
+    
+    function __construct( $_idMasterUserAccount, $_name = null, $_masterUserAccountRole = null, 
+                          $_idParentGroup = null, $_status = null, $_id = null) 
+    {
+        $this->idMasterUserAccount = $_idMasterUserAccount;
+        $this->name = $_name;
+        $this->idParentGroup = $_idParentGroup;
+        $this->masterUserAccountRole = $_masterUserAccountRole;
+        $this->id = $_id;       
+        $this->status = $_status;
+    }
+    
+    public function AddRelatedDocTemplates($docs)
+    {
+        $this->relatedDocumentTemplates = $docs;    
+    }
+    
+    public function GetRelatedDocTemplates()
+    {
+        if(count($this->relatedDocumentTemplates)>0) 
+            return $this->relatedDocumentTemplates;
+        else
+            return false;
+            
+    }
+}
+
+?>
---
+++ Objects/UserGroupDocTemplates.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Object from groups_docs table
+ *
+ * @author Dmitry
+ */
+class UserGroupDocTemplates 
+{
+    var $id;
+    var $name;
+    var $idUserGroups;
+    var $startDate;
+    var $endDate;
+    
+    var $idDocTemplate;
+    var $status;
+    var $docTemplateType; //??
+}
+
+?>
